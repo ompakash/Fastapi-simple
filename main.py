63c27f2a2a70e2d87b0a6557d7eaac54ceeb5a5c
ompakash <opytc2@gmail.com>
2024-04-28 23:06:58 +0530

    get single post

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ my_posts = [{"title":"title of post 1", "content": "content of post 1", "id":1},
             {"title":"title of post 2", "content":"content of post 2", "id":2}]
 
 
+def get_one_post(id):
+    for post in my_posts:
+        if post['id'] == id:
+            return post
+
+
 @app.get("/")
 def read_root():
     return {"message": "World is good"}
@@ -32,4 +38,9 @@ def craete_posts(new_post : Post):
     post_dict['id'] = randrange(1,10000)
     my_posts.append(post_dict)
     return {"new_post": new_post}
+
+@app.get("/posts/{id}")
+def get_post(id:int):
+    post = get_one_post(id)
+    return {"post_details":post}
 
