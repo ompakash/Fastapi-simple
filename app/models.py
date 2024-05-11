6b9285123357a68b4c4b2f469ce5bc530bc95f06
ompakash <opytc2@gmail.com>
2024-05-11 23:04:30 +0530

    add created_at fiedl

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from .database import Base
 from sqlalchemy import String,Integer,Boolean, Column
-from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.expression import null,text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 
 class Post(Base):
     __tablename__ = 'posts'
@@ -8,5 +9,5 @@ class Post(Base):
     id = Column(Integer,primary_key=True,nullable=False)
     title = Column(String,nullable=False)
     content = Column(String,nullable=False)
-    published = Column(Boolean,nullable=True)
-    
+    published = Column(Boolean,server_default='TRUE' ,nullable=True)
+    created_at = Column(TIMESTAMP(timezone=True),server_default= text('now()'),nullable=False)
